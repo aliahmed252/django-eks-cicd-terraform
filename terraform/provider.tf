0eeb3090f6dc1b0b209ae0cbcf2df46be5cc2306
Ali ahmed <aliahmed@fedora>
2026-05-03 02:27:09 +0300

    fix errors

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,35 +1,53 @@
 # provider.tf
 
 terraform {
+  required_version = ">= 1.5.0" # إصدار تيرفورم مستقر
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.42.0" # Specify a compatible version
+      version = "~> 5.0" # استخدام الإصدار 5 المستقر بدلاً من 6
     }
-
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.24.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
-  required_version = ">= 1.14.0"
 }
 
 provider "aws" {
   region = var.aws_region
 }
 
-# جلب بيانات الـ Cluster اللي انت عملته
-data "aws_eks_cluster_auth" "main" {
+# جلب بيانات الـ Cluster بعد إنشائه للتعامل مع الـ Auth
+data "aws_eks_cluster" "cluster" {
   name = aws_eks_cluster.main.name
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.main.name
+}
+
+# إعداد الـ Helm ليتمكن من تنصيب ArgoCD و Prometheus
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.main.token
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
+# إعداد الـ Kubernetes للتعامل مع الـ Resources مباشرة
 provider "kubernetes" {
-  host                   = aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.main.token
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
